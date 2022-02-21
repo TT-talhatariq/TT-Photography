49ddcda017bda1e35110b00805391bdf3b5251c4
Talha Tariq <talhacoder01@gmail.com>
2022-02-21 16:35:24 +0500

    Project Completed

---
+++ main.js
@@ -0,0 +1,12 @@
+// Animate Smooth Scroll
+$('#view-images').on('click', function() {
+    const images = $('#images').position().top;
+  
+    $('html, body').animate(
+      {
+        scrollTop: images
+      },
+      900
+    );
+  });
+  
